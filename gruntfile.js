08f9bdb07b6f46439de149425f55a69763302728
mitchthorson <mthorson@usatoday.com>
2015-08-01 18:47:51 -0400

    fix module layout

--- gruntfile.js
+++ gruntfile.js
@@ -330,7 +330,7 @@ module.exports = function(grunt) {
       },
       upload1: {
         files: {
-          '/17200/experiments/usatoday/2015/07/fb-meter/': 'js/main.js'
+          // '/17200/experiments/usatoday/2015/07/fb-meter/': 'js/main.js'
         }
       },
       upload2: {
@@ -377,6 +377,6 @@ module.exports = function(grunt) {
 
   grunt.registerTask('default', ['clean:dev', 'jst', 'jshint', 'requirejs:dev', 'sass:dev', 'autoprefixer:dev', 'copy:main', 'clean:tmp', 'browserSync:dev', 'watch']);
   grunt.registerTask('test', ['clean:dev', 'jst', 'jshint', 'requirejs:dev', 'sass:dev', 'autoprefixer:dev', 'copy:main', 'copy:test', 'clean:tmp', 'browserSync:test', 'watch']);
-  grunt.registerTask('build', ['clean:dev', 'jst', 'jshint', 'requirejs:deploy', 'requirejs:embed', 'sass:build', 'autoprefixer:build', 'copy:main', 'clean:tmp']);
+  grunt.registerTask('build', ['clean:dev', 'jst', 'jshint', 'requirejs:embed', 'sass:build', 'autoprefixer:build', 'copy:main', 'clean:tmp']);
   grunt.registerTask('deploy', ['build', 'copy:deploy', 'ftp:upload1', 'ftp:upload2', 'ftp:upload3', 'ftp:upload4', 'clean:deploy']);
 };
